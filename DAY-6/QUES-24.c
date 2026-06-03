c3b477b03cf72120d209ad883d6a1407398ee5ed
Ananya Yadav <ananyayadav22.assisi@gmail.com>
2026-06-02 23:04:17 -0700

    Day-6 uplodaded.

--- DAY-6/QUES-24.c
+++ DAY-6/QUES-24.c
@@ -3,7 +3,7 @@ int main(){
     int i=1,n,x;
     printf("Enter the base : ");
     scanf("%d",&x);
-    printf("Enter the raised power : ");
+    printf("Enter the raised power of base : ");
     scanf("%d",&n);
 
     while(n>0){
